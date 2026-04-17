94917441cb08b47f9f099af5687241b3a23ab506
Ryan Wild <ryanwild@users.noreply.github.com>
2026-04-17 11:42:29 +0900

    fixing deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 frontend_path="$(dirname "$(realpath "$0")")"
 $(cd "$frontend_path" && ./build-css.sh && zola build --base-url "https://ryanwild.github.io/kidshub_web/")
 
+git add "$frontend_path/public"
+git commit -m "Deployment: $(git rev-parse --verify HEAD)"
+git push origin
+
 # git switch main
 git subtree split --prefix "frontend/public" -b gh-pages # create a local gh-pages branch containing the splitted output folder
 git push -f origin gh-pages:gh-pages                     # force the push of the gh-pages branch to the remote gh-pages branch at origin
